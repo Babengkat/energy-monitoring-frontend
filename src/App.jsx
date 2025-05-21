70425268c12287e42eedd24cbff5f56662d0e538
Babengkat <autentico.ira@gmail.com>
2025-05-22 00:46:43 +0800

    change

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       return;
     }
 
-    axios.get('http://127.0.0.1:8000/api/energy-readings/', {
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/energy-readings/`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
